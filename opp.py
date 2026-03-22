9f10e87ddc8abc5b6eec4206409ffd2e1faa4084
g4146297-prog <g4146297@gmail.com>
2026-03-22 23:56:49 +0900

    Update opp.py

--- opp.py
+++ opp.py
@@ -4,11 +4,11 @@ import streamlit.components.v1 as components
 # ページ全体を広く使う設定
 st.set_page_config(layout="wide", page_title="思考マップ")
 
-# Streamlitのデフォルトの余白を消すためのCSS
+# Streamlitのヘッダーと被らないように、上部だけ少し余白（3rem）を持たせる
 st.markdown("""
     <style>
         .block-container {
-            padding-top: 0rem;
+            padding-top: 3rem;
             padding-bottom: 0rem;
             padding-left: 0rem;
             padding-right: 0rem;
@@ -16,11 +16,10 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-# mindmap.html を読み込んで画面いっぱいに表示する
+# mindmap.html を読み込んで表示
 try:
     with open("mindmap.html", "r", encoding="utf-8") as f:
         html_data = f.read()
-        # heightを指定してスクロールバーが出ないように調整
-        components.html(html_data, height=900, scrolling=False)
+        components.html(html_data, height=850, scrolling=False)
 except FileNotFoundError:
     st.error("mindmap.html が見つかりません。同じディレクトリに配置されているか確認してください。")
